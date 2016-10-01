6f0195520245cd75629b84400ec4bbc13dbcae6d
Somesh Aryan <somesh.aryan26@gmail.com>
2016-10-01 17:54:26 +0530

    New changes

---
+++ app/src/main/java/com/drspare/www/drspare/BookingHistory.java
@@ -0,0 +1,7 @@
+package com.drspare.www.drspare;
+
+/**
+ * Created by $oMe$H ArYaN on 28-09-2016.
+ */
+public class BookingHistory {
+}
---
+++ app/src/main/java/com/drspare/www/drspare/QuickstartPreferences.java
@@ -0,0 +1,8 @@
+package com.drspare.www.drspare;
+
+/**
+ * Created by $oMe$H ArYaN on 28-09-2016.
+ */
+public class QuickstartPreferences {
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+}
